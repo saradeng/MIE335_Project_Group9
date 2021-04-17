8d16ab904fb139c1e378c2cc1d20f120173d1d54
saradeng <hantao.deng@mail.utoronto.ca>
2021-04-17 19:38:24 -0400

    Added test cases for astar time plot and BDDsolvesetcover time plot

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import time
 from math import gcd
 import BDDSolveSetCover_Group9 as solver
+import RandomInstanceGenerator_Group9 as RIG
 from graphviz import Digraph
+import matplotlib.pyplot as plt
 
 # draw_graph takes as an argument a list of arcs, where an 
 # arc is a 3d tuple: (source, destination, type)
@@ -21,13 +23,54 @@ def draw_graph(arcs_dict, path):
     
     G.view()
 
+def plot_graph(n):
+    d = 0.5
+    mean_times = []
+    max_times = []
+    astar_mean_times = []
+    astar_max_times = []
+    for i in range(2, n+1):
+        print(i)
+        times_n = []
+        astar_times_n = []
+        for j in range(10):
+            U = 10
+            m = 10 * i
+            RIG.RandomInstanceGenerator(i,m,U,d)
+            start = time.time()
+            arcs_dict, path, astar_time = solver.BDDSolveSetCover("test.txt")
+            end = time.time()
+            elapsed_time = end - start
+            times_n.append(elapsed_time)
+            astar_times_n.append(astar_time)
+            
+        #Calculate mean and max for times_n
+        mean = sum(times_n) / len(times_n)
+        mean_times.append(mean)
+        max_times.append(max(times_n))
+        mean = sum(astar_times_n) / len(astar_times_n)
+        astar_mean_times.append(mean)
+        astar_max_times.append(max(astar_times_n))
+        
+        
+    
+    plt.plot(range(2, n+1), mean_times, "-b", label="mean time")
+    plt.plot(range(2, n+1), max_times, "-r", label="max time")
+    plt.legend(loc="upper left")
+    plt.xlabel("size (n)")
+    plt.ylabel("mean/max elapsed time for each n")
+    plt.show()
+    
+    plt.plot(range(2, n+1), astar_mean_times, "-b", label="astar mean time")
+    plt.plot(range(2, n+1), astar_max_times, "-r", label="astar max time")
+    plt.legend(loc="upper left")
+    plt.xlabel("size (n)")
+    plt.ylabel("mean/max elapsed time for each n")
+    plt.show()
+    
+    
 def main():
-    arcs_dict, path = solver.BDDSolveSetCover("Checkpoint4_instances/example.txt")
-    # arcs_dict, path = solver.BDDSolveSetCover("Checkpoint4_instances/50_500_scpe1.txt")
-    # arcs_dict, path = solver.BDDSolveSetCover("Checkpoint4_instances/240_192_scpcyc06.txt")
-    print(arcs_dict)
-    print(path)
-    draw_graph(arcs_dict, path)
-
+    plot_graph(5)
+    
 if __name__ == "__main__":
     main()
